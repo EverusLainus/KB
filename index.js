7be3a0641166fc477cb14e7b325b38f30c7eeb26
Everus <everuslainus@gmail.com>
2024-09-22 23:24:57 +0530

    authentication

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const authRouter = require("./routes/authRoutes");
 const mongodbConnect = require("./db/connection");
 mongodbConnect();
 app.use(express.json());
+app.use("/auth", authRouter);
 app.listen(3000, () => {
   console.log("server started at http://localhost:3000");
 });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const userModel = require("../models/userModel");
 const authRouter = express.Router();
@@ -20,19 +23,29 @@ authRouter.post("/register", async (req, res) => {
 authRouter.post("/login", async (req, res) => {
   const { name, password, role } = req.body;
   try {
-    const user = await userModel.find({ name });
+    const user = await userModel.findOne({ name });
     if (!user) {
       return res.send({ message: "error: invalid username" });
     }
-    const isCorrect = bcrypt.compare(password, user.password);
+    console.log(user);
+    console.log(typeof user.password);
+    const isCorrect = await bcrypt.compare(password, user.password);
+    console.log(isCorrect);
     if (!isCorrect) {
       return res.send({ message: "error: invalid password" });
     }
-    const token = bcrypt.sign({ id: user._is, role: user.role });
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.jwt_secret,
+      {
+        expiresIn: "1 hour",
+      }
+    );
+    console.log(token);
     res.status(200).json({ token });
   } catch (err) {
     console.log(err.message);
-    res.send({ message: "error: login" });
+    return res.send({ message: "error: login" });
   }
 });
 
